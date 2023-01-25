c993bc374789446872f8aaeafc85f878f7d7b088
gclncguy@gmail.com <Dexterde1>
2023-01-25 16:00:53 +0000

    Initial commit

---
+++ build/util.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getMessage = void 0;
+function getMessage() {
+    return "hello";
+}
+exports.getMessage = getMessage;
---
+++ util.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getMessage = void 0;
+function getMessage() {
+    return "hello";
+}
+exports.getMessage = getMessage;
+//# sourceMappingURL=util.js.map
